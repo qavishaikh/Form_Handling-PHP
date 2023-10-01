9841cddf32531245c5cc5233c7ca437c0f6a8ec1
Qavi <qavi.shaikh11@gmail.com>
2023-10-01 11:30:19 +0500

    Form Handling

---
+++ output.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <tittle>Form Handling OutPut</tittle>
+    </head>
+    <body>
+        <h1>OutPut</h1>
+      <h4>  Welcome <?php echo $_POST["name"]; ?> </h4> 
+       <h4> Your email address is: <?php echo $_POST["email"]; ?> </h4>
+    </body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <tittle>Form Handling</tittle>
+    </head>
+    <body>
+        <form action="output.php" method="POST">
+            <h1>Welcome page</h1>
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Enter Your Name"> <br>
+            <label>Email</label>
+            <input type="email" name="email" placeholder="Enter Your Email Address"> <br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
