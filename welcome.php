60fecfb8b98e1b55cd07e51311de626cc660ab0e
Qavi <qavi.shaikh11@gmail.com>
2023-10-01 11:54:48 +0500

    Form handling With Styling

--- welcome.php
+++ welcome.php
@@ -1,15 +1,20 @@
 <html>
     <head>
         <tittle>Form Handling</tittle>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <div class="container">
         <form action="output.php" method="POST">
             <h1>Welcome page</h1>
             <label>Name</label>
-            <input type="text" name="name" placeholder="Enter Your Name"> <br>
+            <input type="text" name="name" placeholder="Enter Your Name"> <br><br>
             <label>Email</label>
-            <input type="email" name="email" placeholder="Enter Your Email Address"> <br>
+            <input type="email" name="email" placeholder="Enter Your Email Address"> <br><br>
+            <label>Mobile Number</label>
+            <input type="number" name="number" placeholder="Enter Your Mobile Number"> <br><br>
             <input type="submit">
         </form>
+        </div>
     </body>
 </html>
